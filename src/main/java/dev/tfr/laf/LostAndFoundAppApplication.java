f0fe2dbdc2228a8f68419b1b625ea84319bbaae9
thiagorabaioli <tfrabaioli@outlook.com>
2024-01-15 12:19:38 +0000

    First commit

---
+++ src/main/java/dev/tfr/laf/LostAndFoundAppApplication.java
@@ -0,0 +1,13 @@
+package dev.tfr.laf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostAndFoundAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostAndFoundAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/tfr/laf/LostAndFoundAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tfr.laf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostAndFoundAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
