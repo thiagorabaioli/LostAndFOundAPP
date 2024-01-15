3cec4a3dd7101834cbdd06f5632a31369c00309c
thiagorabaioli <tfrabaioli@outlook.com>
2024-01-15 12:39:41 +0000

    JPA persistence

--- src/main/java/dev/tfr/laf/entities/Lost.java
+++ src/main/java/dev/tfr/laf/entities/Lost.java
@@ -1,9 +1,16 @@
 package dev.tfr.laf.entities;
 
+import dev.tfr.laf.enums.TYPELOST;
+import jakarta.persistence.*;
+
 import java.util.Date;
 
+@Entity
+@Table(name= "tb_lost")
 public class Lost {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String description;
     private TYPELOST typeCod;
@@ -13,4 +20,73 @@ public class Lost {
 
     private Date instantFind;
 
+    @ManyToOne
+    @JoinColumn(name="userApp_id")
+    private UserAPP userApp;
+
+    public Lost(Long id, String description, TYPELOST typeCod, String whoFind, String localFind, Date instantFind, UserAPP userApp) {
+        this.id = id;
+        this.description = description;
+        this.typeCod = typeCod;
+        this.whoFind = whoFind;
+        this.localFind = localFind;
+        this.instantFind = instantFind;
+        this.userApp = userApp;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public TYPELOST getTypeCod() {
+        return typeCod;
+    }
+
+    public void setTypeCod(TYPELOST typeCod) {
+        this.typeCod = typeCod;
+    }
+
+    public String getWhoFind() {
+        return whoFind;
+    }
+
+    public void setWhoFind(String whoFind) {
+        this.whoFind = whoFind;
+    }
+
+    public String getLocalFind() {
+        return localFind;
+    }
+
+    public void setLocalFind(String localFind) {
+        this.localFind = localFind;
+    }
+
+    public Date getInstantFind() {
+        return instantFind;
+    }
+
+    public void setInstantFind(Date instantFind) {
+        this.instantFind = instantFind;
+    }
+
+    public UserAPP getUserApp() {
+        return userApp;
+    }
+
+    public void setUserApp(UserAPP userApp) {
+        this.userApp = userApp;
+    }
 }
--- src/main/java/dev/tfr/laf/entities/UserAPP.java
+++ src/main/java/dev/tfr/laf/entities/UserAPP.java
@@ -1,14 +1,71 @@
 package dev.tfr.laf.entities;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "TB_user_app")
 public class UserAPP {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
     private String porNumber;
 
+    @OneToMany(mappedBy = "userApp")
+    private List<Lost> losts = new ArrayList<>();
+
 
     public UserAPP(){};
 
+    public UserAPP(Long id, String name, String email, String porNumber) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.porNumber = porNumber;
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPorNumber() {
+        return porNumber;
+    }
+
+    public void setPorNumber(String porNumber) {
+        this.porNumber = porNumber;
+    }
+
+    public List<Lost> getLosts() {
+        return losts;
+    }
+
+    public void setLosts(List<Lost> losts) {
+        this.losts = losts;
+    }
 }
